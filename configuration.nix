ff62969d7af041bf7ae8f3182c44bf03afd0fbaf
d-kholin <michael@thegriffiths.ca>
2026-05-29 09:19:07 -0600

    feat: add bat and bmon

--- configuration.nix
+++ configuration.nix
@@ -122,6 +122,8 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     bitwarden-desktop
+    bat
+    bmon
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
