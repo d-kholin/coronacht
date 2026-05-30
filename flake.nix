24b42ce31258f13b7944a2f38afc561e7e48f06a
d-kholin <michael@thegriffiths.ca>
2026-05-29 23:09:52 -0600

    chore fix gitconfig

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "bak";
           home-manager.users.michael = import ./home.nix;
         }
       ];
