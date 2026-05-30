aa078a2b4301c69121ebd443293e9faa57528378
d-kholin <michael@thegriffiths.ca>
2026-05-29 23:09:34 -0600

    chore: update old to new nomenclature

--- home.nix
+++ home.nix
@@ -4,23 +4,31 @@
   home.username = "michael";
   home.homeDirectory = "/home/michael";
   home.stateVersion = "25.11";
+  home.enableNixpkgsReleaseCheck = false;
 
   programs.ssh = {
     enable = true;
-    matchBlocks = {
-      "automate" = {
-        host = "automate.thegriffiths.ca automate";
-        hostname = "172.20.0.221";
-        user = "michael";
+    settings = {
+      "automate.thegriffiths.ca automate" = {
+        HostName = "172.20.0.221";
+        User = "michael";
+      };
+      "ns1.thegriffiths.ca ns1" = {
+        HostName = "172.20.0.3";
+        User = "michael";
+      };
+      "ns2.thegriffiths.ca ns2" = {
+        HostName = "172.20.0.4";
+        User = "michael";
       };
     };
   };
 
   programs.git = {
     enable = true;
-    userName = "d-kholin";
-    userEmail = "michael@thegriffiths.ca";
-    extraConfig = {
+    settings = {
+      user.name = "d-kholin";
+      user.email = "michael@thegriffiths.ca";
       gpg.format = "ssh";
       gpg.ssh.allowedSignersFile = "/etc/git/allowed_signers";
       user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB0UuxPDrNThrfQXcdwAIr2ZZ5TcrL6cdDdjW/z5cNYa MG-General";
